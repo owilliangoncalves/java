4a90951f18d2d78b8c7c976cd84129e8364238f5
Willian Gonçalves <williangoncalves74@gmail.com>
2025-07-30 16:45:16 -0300

    criado classes para cadastrar produto e gerenciamento de estoque

--- src/Desafios/Classes/Desafio1/Produto.java
+++ src/Desafios/Classes/Desafio1/Produto.java
@@ -1,4 +1,38 @@
 package Desafios.Classes.Desafio1;
 
 public class Produto {
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    public Produto(String nome, double preco, int quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public boolean vender(int qtd) {
+        if (this.quantidade >= qtd) {
+            this.quantidade -= qtd;
+            return true;
+        }
+        return false;
+    }
+
+    public void adicionarEstoque(int qtd) {
+        if (qtd > 0) {
+            this.quantidade += qtd;
+        } else {
+            throw new IllegalArgumentException("Quantidade precisa ser maior que zero.");
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void exibirInfo() {
+        System.out.printf("Produto: %s | Preço: R$ %.2f | Estoque: %d\n",
+                nome, preco, quantidade);
+    }
 }
